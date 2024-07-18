0bb6bf26647a0fe9da3bea734fff7f85c417edd6
jaceleedev <jaceleedev@gmail.com>
2024-07-18 16:17:41 +0900

    feat: :lipstick: add styles for QRCode component

--- src/components/QRCode/index.tsx
+++ src/components/QRCode/index.tsx
@@ -1,19 +1,25 @@
 import Image from 'next/image';
+import styles from './index.module.css';
 
 function QRCode() {
   return (
-    <div>
+    <div className={styles['qr-code']}>
       <Image
         src={'/images/image-qr-code.png'}
         alt="QR code"
         width={288}
         height={288}
+        className={styles['qr-code__image']}
       />
-      <h1>Improve your front-end skills by building projects</h1>
-      <p>
-        Scan the QR code to visit Frontend Mentor and take your coding skills to
-        the next level
-      </p>
+      <div className={styles['qr-code__text-box']}>
+        <h1 className={styles['qr-code__heading']}>
+          Improve your front-end skills by building projects
+        </h1>
+        <p className={styles['qr-code__body']}>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
     </div>
   );
 }
